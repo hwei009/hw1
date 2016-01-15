4787d3a0bf7b68e4495d4ab79a0562f198c7e2e1
hwei009 <Haining@wireless-s0-so-150-42-163.uchicago.edu>
2016-01-14 19:12:37 -0600

    Test

---
+++ solution_8.py
@@ -0,0 +1,10 @@
+#covert.py
+#   A program to convert Fahrenheit temps to Celsius
+#by Haining(Helen) Wei
+
+def main():
+    fahrenheit=eval(input("What is the Feharenheit temperature? "))
+    celsius=5/9 * fahrenheit - 160/9
+    print("The temperature is", celsius, "degrees Celsius.")
+
+main()
